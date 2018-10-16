05ffd7e497a8de9d14d35d07257ec7c2a7426c18
tssqzr <1214661466@qq.com>
2018-10-16 10:05:29 +0800

    Paging processing data

--- ContractNotifyCollector/contract/task/AuctionStateUpdateTask.cs
+++ ContractNotifyCollector/contract/task/AuctionStateUpdateTask.cs
@@ -53,6 +53,7 @@ namespace ContractNotifyCollector.core.task
             timeSetter = TimeConst.getTimeSetter(root);
             // db info
             localDbConnInfo = Config.localDbConnInfo;
+
             //
             initSuccFlag = true;
         }
@@ -66,7 +67,7 @@ namespace ContractNotifyCollector.core.task
 
                 try
                 {
-                    updateState();
+                    updateStateNew();
                 } catch (Exception ex)
                 {
                     // 发生异常不需要退出线程
@@ -74,6 +75,127 @@ namespace ContractNotifyCollector.core.task
                 }
             }
         }
+        
+        private void updateStateNew()
+        {
+            // 竞拍域名状态更新
+            long nowtime = TimeHelper.GetTimeStamp();
+            JObject FiveDayFilter = MongoFieldHelper.toFilter(new string[] { AuctionState.STATE_START, AuctionState.STATE_CONFIRM, AuctionState.STATE_RANDOM }, "auctionState");
+            string filter = FiveDayFilter.ToString();
+            long count = mh.GetDataCount(localDbConnInfo.connStr, localDbConnInfo.connDB, auctionStateColl, filter);
+            if (count > 0)
+            {
+                //
+                for (int startIndex = 0; startIndex < count; startIndex += batchSize)
+                {
+                    List<AuctionTx> list = mh.GetData<AuctionTx>(localDbConnInfo.connStr, localDbConnInfo.connDB, auctionStateColl, filter, "{}", startIndex, batchSize);
+                    if (list == null || list.Count == 0) { continue; }
+
+                    updateState(list, nowtime);
+                }
+            }
+
+            // 过期域名状态更新
+            JObject OneYearFilter = new JObject() { { "auctionState", AuctionState.STATE_END }, { "startTime.blocktime", new JObject() { { "$gt", nowtime - timeSetter.ONE_YEAR_SECONDS } } } };
+            filter = OneYearFilter.ToString();
+            count = mh.GetDataCount(localDbConnInfo.connStr, localDbConnInfo.connDB, auctionStateColl, filter);
+            if (count > 0)
+            {
+                //
+                int pageNum = 0;
+                for (int startIndex = 0; startIndex < count; startIndex += batchSize)
+                {
+
+                    JArray ja = mh.GetDataPagesWithField(localDbConnInfo.connStr, localDbConnInfo.connDB, auctionStateColl, new JObject() { { "auctionId", 1 } }.ToString(), ++pageNum, batchSize, "{}", filter);
+                    if (ja == null || ja.Count == 0) { continue; }
+
+                    string subfilter = MongoFieldHelper.toFilter(ja.Select(p => p["auctionId"].ToString()).ToArray(), "auctionId").ToString();
+                    string newdata = new JObject() { { "$set", new JObject() { { "auctionState", AuctionState.STATE_EXPIRED } } } }.ToString();
+                    mh.UpdateData(localDbConnInfo.connStr, localDbConnInfo.connDB, auctionStateColl, newdata, subfilter);
+                }
+            }
+            
+        }
+        private void updateState(List<AuctionTx> list, long nowtime)
+        {
+            foreach (AuctionTx jo in list)
+            {
+                if (root != "all" && !jo.fulldomain.EndsWith(root)) continue;
+                long starttime = jo.startTime.blocktime;
+                string oldState = jo.auctionState;
+                string newState = null;
+                long endTimeBlocktime = 0;
+
+                // 结束并且超过1Y，直接更新状态
+                if (jo.auctionState == AuctionState.STATE_END)
+                {
+                    newState = AuctionState.STATE_EXPIRED;
+                    updateAuctionState(newState, oldState, jo.auctionId);
+                    continue;
+                }
+
+                /**
+                    * 状态判断逻辑：
+                    * 0. 开标为开标期
+                    * 1. 小于等于三天，确定期
+                    * 2. 大于三天，则：
+                    *          a. 结束时间无值且前三天无人出价，则流拍
+                    *          b. 超过1Y，则过期
+                    *          c. 超过5D，则结束
+                    *          d. (3,5)结束时间有值且大于开拍时间，则结束
+                    *          e. (3,5)结束时间无值且最后出价在开拍后两天内，则结束
+                    *          f. (3,5)其余为随机
+                    */
+                if (nowtime - starttime <= timeSetter.THREE_DAY_SECONDS)
+                {
+                    // 小于三天
+                    newState = AuctionState.STATE_CONFIRM;
+                }
+                else
+                {
+                    // 大于三天
+                    if (jo.lastTime == null || jo.lastTime.blockindex == 0 || jo.addwholist == null || jo.addwholist.Count == 0)
+                    {
+                        // (3,5)结束时间无值且前三天无人出价，则流拍
+                        newState = AuctionState.STATE_ABORT;
+                    }
+                    else if (nowtime > starttime + timeSetter.ONE_YEAR_SECONDS)
+                    {
+                        // 超过1Y，则过期
+                        newState = AuctionState.STATE_EXPIRED;
+                    }
+                    else if (nowtime >= starttime + timeSetter.FIVE_DAY_SECONDS)
+                    {
+                        // 超过5D，则结束
+                        newState = AuctionState.STATE_END;
+                        endTimeBlocktime = starttime + timeSetter.FIVE_DAY_SECONDS;
+                    }
+                    else if (jo.endTime != null && jo.endTime.blocktime > starttime)
+                    {
+                        // (3,5)结束时间有值，且大于开拍时间，则结束
+                        newState = AuctionState.STATE_END;
+                    }
+                    else if (jo.lastTime.blocktime <= starttime + timeSetter.TWO_DAY_SECONDS)
+                    {
+                        // (3,5)结束时间无值且最后出价在开拍后两天内，则超时3D结束
+                        newState = AuctionState.STATE_END;
+                    }
+                    else
+                    {
+                        // 其余为随机期
+                        newState = AuctionState.STATE_RANDOM;
+                    }
+                }
+
+                if (oldState != newState)
+                {
+                    updateAuctionState(newState, oldState, jo.auctionId, endTimeBlocktime);
+                }
+
+            }
+            log(list.Count());
+        }
+
 
         private void updateState()
         {
